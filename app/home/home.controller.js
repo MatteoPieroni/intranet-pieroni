b7a1d3d149aa32df780cdc4e46262cc615e026dc
Matteo Pieroni <matteopieroni6@gmail.com>
2018-06-02 02:04:11 +0200

    Firebase
    Added Firebase to keep links in a backend and created link cards based on the database

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,17 +1,39 @@
 (function () {
 
   'use strict';
-
   angular
     .module('app')
     .controller('HomeController', homeController);
 
-  homeController.$inject = ['authService'];
+  homeController.$inject = ['authService', '$scope', '$firebaseAuth', '$firebaseObject', '$firebaseArray'];
 
-  function homeController(authService) {
-
+  function homeController(authService, $scope, $firebaseAuth, $firebaseObject, $firebaseArray) {
     var vm = this;
     vm.auth = authService;
+
+    if(vm.auth.isAuthenticated) {
+    	  // Initialize Firebase
+		  var config = {
+		    apiKey: "AIzaSyA7XzDXxEuhPoLwD3l02qcHeLWovVCAH-Y",
+		    authDomain: "intranet-pieroni.firebaseapp.com",
+		    databaseURL: "https://intranet-pieroni.firebaseio.com",
+		    projectId: "intranet-pieroni",
+		    storageBucket: "intranet-pieroni.appspot.com",
+		    messagingSenderId: "775811721929"
+		  };
+		  firebase.initializeApp(config);
+
+		  var ref = firebase.database().ref('links/');
+		  $scope.links = $firebaseObject(ref);
+
+		  $scope.links.$loaded()
+		  .then(function() {
+		    console.log($scope.links);
+		  })
+		  .catch(function(err) {
+		    console.error(err);
+		  });
+    }
 
   }
 
