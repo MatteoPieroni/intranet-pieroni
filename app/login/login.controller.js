8a473906ac345f12c34256eb76d0121ed17b0d0c
Matteo Pieroni <matteopieroni6@gmail.com>
2018-06-25 02:42:26 +0200

    Update login
    Added reset password functionality and styled accordingly

--- app/login/login.controller.js
+++ app/login/login.controller.js
@@ -6,9 +6,9 @@
     .module('app')
     .controller('LoginController', loginController);
 
-  loginController.$inject = ['$rootScope', '$scope', 'Auth', 'currentAuth', '$state'];
+  loginController.$inject = ['$rootScope', '$scope', 'Auth', 'currentAuth', '$state', '$timeout'];
 
-  function loginController($rootScope, $scope, Auth, currentAuth, $state) {
+  function loginController($rootScope, $scope, Auth, currentAuth, $state, $timeout) {
 
     var vm = this;
 
@@ -32,7 +32,33 @@
           }
           console.error("Authentication failed:", error);
       });
+    };
+    // Init reset as false for UI
+    $scope.resetting = false;
+    $scope.resetLoader = false;
+    $scope.resetForm = {};
+    // Function for showing resetting UI
+    $scope.showReset = function() {
+      $scope.resetting = true;
     }
+    // Function for sending password
+    $scope.resetPassword = function() {
+      var email = $scope.resetForm.emailReset;
+      $scope.resetLoader = true;
+      Auth.$sendPasswordResetEmail(email).then(function() {
+        $scope.resetMessage = "Ti abbiamo inviato una mail per il reset della password!";
+        $scope.resetForm.emailReset = '';
+        $scope.resetLoader = false;
+        $timeout( function(){
+          $scope.resetMessage = '';
+          $scope.resetting = false;
+        }, 3000 );
+      }).catch(function(error) {
+        $scope.resetMessage = "Sembra esserci stato un errore. Contatta l'amministratore! Descrizione dell'errore: " + error;
+        $scope.resetLoader = false;
+      });
+    }
+
     Auth.$onAuthStateChanged(function(firebaseUser, toState, toParams, fromState, fromParams) {
       if(firebaseUser) {
         $state.go('home');
