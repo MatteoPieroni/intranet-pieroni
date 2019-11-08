85e120b0a73ca12aee8eefe969b0e280a4321ef2
Matteo Pieroni <matteopieroni6@gmail.com>
2019-11-08 21:38:45 +0000

    Added test settings and one test

--- src/utils/normaliseObjectKeysToArray.test.ts
+++ src/utils/normaliseObjectKeysToArray.test.ts
@@ -1,7 +1,19 @@
 import { normaliseObjectKeysToArray } from './normaliseObjectKeysToArray';
 
+const mockObject = {
+  test1: 'test 1',
+  test2: 'test 2',
+  test3: 'test 3',
+};
+
+const resultingArray = ['test 1', 'test 2', 'test 3'];
+
 describe('normaliseObjectKeysToArray', () => {
   it('returns null if not passed an object', () => {
     expect(normaliseObjectKeysToArray(null)).toEqual(null);
+  });
+
+  it('returns an array with the values of an object keys (like Object.values)', () => {
+    expect(normaliseObjectKeysToArray(mockObject)).toEqual(resultingArray);
   });
 });
