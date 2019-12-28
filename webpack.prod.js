b088235cb59a76f2e99abeeaa35c5ed9e61b3c63
Matt <matteopieroni6@gmail.com>
2019-12-28 22:05:43 +0000

    Switched to splitting vendor chunk and obfuscating the rest

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,28 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const merge = require('webpack-merge');
 const webpack = require('webpack');
+const JavaScriptObfuscator = require('webpack-obfuscator');
 
 const common = require('./webpack.config');
 
 module.exports = merge(common, {
   mode: 'production',
+
+  optimization: {
+    runtimeChunk: 'single',
+    namedChunks: true,
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendor'
+        },
+      },
+    },
+  },
 
   plugins: [
     new webpack.DefinePlugin({
@@ -17,6 +34,7 @@ module.exports = merge(common, {
       'process.env.MESSAGING_SENDER_ID': JSON.stringify(process.env.MESSAGING_SENDER_ID),
       'process.env.APP_ID': JSON.stringify(process.env.APP_ID),
       'process.env.SMS_API': JSON.stringify(process.env.SMS_API),
-    })
+    }),
+    new JavaScriptObfuscator ({}, ['vendor.index_bundle.js'])
   ],
 })
