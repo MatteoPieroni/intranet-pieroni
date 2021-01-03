6744838c3c81debfac6682d56d1b2052b050b569
Matteo Pieroni <matteopieroni6@gmail.com>
2021-01-03 16:34:37 +0000

    Added data normalisation

---
+++ src/utils/file-system/normaliser.ts
@@ -0,0 +1,72 @@
+import { IFile, IFolder } from "../../services/firebase/types";
+
+interface IOrganisedFolders {
+	[key: string]: {
+		name: string;
+	};
+}
+interface IOrganisedData {
+	[key: string]: {
+		name: string;
+		files: IFile[];
+		subfolders?: IOrganisedFolders;
+	};
+}
+
+const homeFolder: {
+	name: string;
+	files: IFile[];
+	subfolders: IOrganisedFolders;
+} = {
+	name: 'Home',
+	files: [],
+	subfolders: {},
+}
+
+export const organiseData: (folders: IFolder[], files: IFile[]) => IOrganisedData = (folders, files) => {
+	const organisedFolders: IOrganisedFolders = folders.reduce((acc, folder) => ({
+		...acc,
+		...(folder && 
+			{
+				[folder.id]: {
+					name: folder.name,
+				}
+			}
+		)
+	}), {});
+
+	const organisedHome = {
+		...homeFolder,
+		subfolders: organisedFolders
+	}
+
+	return files.reduce((acc: IOrganisedData, file) => {
+		if (file.folder) {
+			return {
+				...acc,
+				[file.folder]: {
+					name: organisedFolders[file.folder].name,
+					files: [
+						...(acc[file.folder]?.files || []),
+						file,
+					],
+				}
+			}
+		}
+
+		return {
+			...acc,
+			home: {
+				...acc.home,
+				files: [
+					...(acc.home?.files || []),
+					file,
+				],
+			}
+		}
+	},
+	// start with the home data already in
+	{
+		home: organisedHome,
+	});
+}
