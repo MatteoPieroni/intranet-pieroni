de9c624621ab77b3355d1b42ec12b58eea7d7bc2
Matteo Pieroni <matteopieroni6@gmail.com>
2021-01-11 18:35:35 +0000

    Moved remove functionality

--- src/services/firebase/dbf/links/links-courier.ts
+++ src/services/firebase/dbf/links/links-courier.ts
@@ -1,6 +1,7 @@
 import * as Types from '../../types';
-import { addRecord, listenToDb, updateRecord } from '../../db';
+import { addRecord, listenToDb, removeRecord, updateRecord } from '../../db';
 
 export const listenToLinks: (callback: (hasError: boolean, data?: Types.ILink[]) => void) => void  = callback => listenToDb('/links', callback);
 export const updateLink: (id: string, data: Types.ILink) => Promise<Types.ILink | Error> = async (id, data) => await updateRecord('/links', id, data);
 export const addLink: (data: Types.ILink) => Promise<Types.ILink | Error> = async (data) => await addRecord('/links', data, true);
+export const removeLink: (id: string) => Promise<void | Error> = async (id) => await removeRecord('/links', id);
