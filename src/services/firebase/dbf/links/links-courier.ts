14352f3d070964be81941686c36bc21083bb829c
Matteo Pieroni <matteopieroni6@gmail.com>
2021-01-11 18:29:05 +0000

    Moved update functions

--- src/services/firebase/dbf/links/links-courier.ts
+++ src/services/firebase/dbf/links/links-courier.ts
@@ -1,4 +1,5 @@
 import * as Types from '../../types';
-import { listenToDb } from '../../db';
+import { listenToDb, updateRecord } from '../../db';
 
 export const listenToLinks: (callback: (hasError: boolean, data?: Types.ILink[]) => void) => void  = callback => listenToDb('/links', callback);
+export const updateLink: (id: string, data: Types.ILink) => Promise<Types.ILink | Error> = async (id, data) => await updateRecord('/links', id, data);
--- src/services/firebase/dbf/quote/quote-courier.ts
+++ src/services/firebase/dbf/quote/quote-courier.ts
@@ -1,5 +1,6 @@
 import * as Types from '../../types';
-import { getDbRecordById } from '../../db';
+import { getDbRecordById, updateRecord } from '../../db';
 
 
 export const getQuote: () => Promise<Types.IQuote> = () => getDbRecordById('/quote', 'active');
+export const updateQuote: (data: Types.IQuote) => Promise<Types.IQuote | Error> = async (data) => await updateRecord('/quote', 'active', data);
