768126474d9ba26befa5822cd482454f41fe9630
Matteo Pieroni <matteopieroni6@gmail.com>
2021-01-11 18:32:57 +0000

    Moved add and sms

--- src/services/firebase/dbf/links/links-courier.ts
+++ src/services/firebase/dbf/links/links-courier.ts
@@ -1,5 +1,6 @@
 import * as Types from '../../types';
-import { listenToDb, updateRecord } from '../../db';
+import { addRecord, listenToDb, updateRecord } from '../../db';
 
 export const listenToLinks: (callback: (hasError: boolean, data?: Types.ILink[]) => void) => void  = callback => listenToDb('/links', callback);
 export const updateLink: (id: string, data: Types.ILink) => Promise<Types.ILink | Error> = async (id, data) => await updateRecord('/links', id, data);
+export const addLink: (data: Types.ILink) => Promise<Types.ILink | Error> = async (data) => await addRecord('/links', data, true);
