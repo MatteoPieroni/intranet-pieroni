2aa343ec5a7c292745b77bd68d7acfec9581b093
Matteo Pieroni <matteopieroni6@gmail.com>
2021-04-23 19:07:52 +0100

    Added guiding comments to normaliser

--- src/utils/file-system/normaliser.ts
+++ src/utils/file-system/normaliser.ts
@@ -9,10 +9,12 @@ export interface ICategoryWithSubfolders extends ICategoryWithFileCount {
 }
 
 export interface IOrganisedCategoriesGeneric<T> {
+	uncategorised?: T;
 	[key: string]: T;
 }
 
 export interface IOrganisedFiles {
+	uncategorised: IFile[];
 	[key: string]: IFile[];
 }
 
@@ -23,7 +25,9 @@ export interface IOrganisedData {
 	categories: IOrganisedCategories;
 }
 
+// This function is used to check for a category at the subfolders levels
 const transverseAndPlace: (tree: IOrganisedCategoriesGeneric<ICategoryWithSubfolders>, current: ICategoryWithFileCount) => IOrganisedCategoriesGeneric<ICategoryWithSubfolders> = (tree, { parent, ...current }) => {
+	// if at the current level we find the parent add the current category to its subfolders
 	if (tree[parent]) {
 		tree[parent].subfolders = {
 			...tree[parent].subfolders,
@@ -36,20 +40,39 @@ const transverseAndPlace: (tree: IOrganisedCategoriesGeneric<ICategoryWithSubfol
 		return tree;
 	}
 
+	// else, for each branch in the current tree
 	for (const branchKey in tree) {
+		// it it has subfolders
 		if (tree[branchKey].subfolders) {
+			// transverse those subfolders
 			transverseAndPlace(tree[branchKey].subfolders, { parent, ...current });
 		}
 	}
 }
 
+const uncategorised: ICategoryWithSubfolders = {
+	id: 'uncategorised',
+	label: 'Senza categoria',
+	parent: '',
+	depth: 0,
+	fileCount: 0,
+	subfolders: null,
+}
+
 export const organiseFolders: (categories: ICategoryWithFileCount[]) => IOrganisedCategoriesGeneric<ICategoryWithSubfolders> = (categories) => {
-	const orderedTree: IOrganisedCategoriesGeneric<ICategoryWithSubfolders> = {};
+	// create a lookup table with each category
+	// it must include the uncategorised value to make sure we catch any "loose" files
+	const orderedTree: IOrganisedCategoriesGeneric<ICategoryWithSubfolders> = {
+		uncategorised,
+	};
+	// order the categories by depth
 	const orderedCategories = categories.sort((curr, next) => curr.depth - next.depth);
 
+	// for each ordered category
 	orderedCategories.forEach(category => {
 		const { parent, ...cat } = category;
 
+		// if there is no parent add it to the root level
 		if (!parent) {
 			orderedTree[cat.id] = {
 				...cat,
@@ -59,6 +82,7 @@ export const organiseFolders: (categories: ICategoryWithFileCount[]) => IOrganis
 			return;
 		}
 
+		// else check the subfolders and place it there
 		transverseAndPlace(orderedTree, category);
 	});
 
@@ -66,10 +90,19 @@ export const organiseFolders: (categories: ICategoryWithFileCount[]) => IOrganis
 }
 
 export const organiseFiles: (files: IFile[]) => IOrganisedFiles = (files) => {
-	const fileList: IOrganisedFiles = {};
+	const fileList: IOrganisedFiles = {
+		uncategorised: [],
+	};
 
+	// for each file
 	files.forEach(file => {
+		if (!file.categoriesId) {
+			fileList.uncategorised.push(file);
+			return;
+		}
+		// loop through categories
 		file.categoriesId.forEach(category => {
+			// add the file to each category id in the final object
 			fileList[category] = [
 				...(fileList[category] || []),
 				file,
@@ -82,6 +115,7 @@ export const organiseFiles: (files: IFile[]) => IOrganisedFiles = (files) => {
 
 export const organiseData: (categories: ICategory[], files: IFile[]) => IOrganisedData =
 	(categories, files) => {
+		// create a lookup table of all the categories
 		const categoriesLookup: IOrganisedCategoriesGeneric<ICategoryWithFileCount> = categories
 			.reduce((acc, current) => ({
 				...acc,
@@ -90,8 +124,19 @@ export const organiseData: (categories: ICategory[], files: IFile[]) => IOrganis
 					fileCount: 0,
 				},
 			}), {});
+		// add the uncategorised cat to make sure we catch all "loose" files
+		categoriesLookup.uncategorised = uncategorised;
 
+		// for each file
 		files.forEach(({ categoriesId }) => {
+			// if it has no categories
+			if (!categoriesId) {
+				// add it to the uncategorised file count in the lookup
+				categoriesLookup.uncategorised.fileCount = categoriesLookup.uncategorised.fileCount + 1;
+				return;
+			}
+
+			// otherwise add it to each category file count in the lookup table
 			categoriesId.forEach(cat => {
 				categoriesLookup[cat].fileCount = categoriesLookup[cat].fileCount + 1;
 			});
