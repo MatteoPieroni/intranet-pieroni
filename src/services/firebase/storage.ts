def0d71bbe9829d6a92890729a09da6fc11d4907
Matteo Pieroni <matteopieroni6@gmail.com>
2021-05-31 17:34:03 +0100

    Fixed access to firebase

--- src/services/firebase/storage.ts
+++ src/services/firebase/storage.ts
@@ -1,10 +1,9 @@
-import * as Types from './db';
 import { fireApp } from './app';
 
 const fireStorage = fireApp.storage();
 
 export const getFileDownloadUrl: (fileUrl: string) => Promise<string> = async (url) => {
-  const ref = fireStorage.refFromURL(url);
+  const ref = fireStorage.ref(url);
   const fireUrl = await ref.getDownloadURL();
 
   return fireUrl;
