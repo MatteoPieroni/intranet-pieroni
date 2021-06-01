c5b165ca79a5962f2d8e0851f87451b66dd7372b
Matteo Pieroni <matteopieroni6@gmail.com>
2021-06-01 18:24:08 +0100

    Added sync status to Catalogues Service

--- src/services/catalogues-api/catalogues-couriers.ts
+++ src/services/catalogues-api/catalogues-couriers.ts
@@ -1,4 +1,5 @@
 import { IFile } from "../firebase/db";
+import { ISyncStatuses } from "./catalogues-service";
 
 export const apiExists: (url: string, token: string) => Promise<boolean> = async (apiUrl, token) => new Promise(async (resolve, reject) => {
 	try {
@@ -47,6 +48,38 @@ export const deleteCatalogue: (url: string, token: string, values: string) => Pr
 				body: JSON.stringify({ id: values }),
 			});
 			const res = await response.json();
+
+			resolve(res);
+		} catch (e) {
+			reject(e);
+		}
+	})
+}
+
+export const syncCatalogues: (url: string, token: string) => Promise<string> = (apiUrl, token) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const response = await fetch(apiUrl, {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'x-access-token': token,
+				},
+			});
+			const res = await response.text();
+
+			resolve(res);
+		} catch (e) {
+			reject(e);
+		}
+	})
+}
+
+export const syncCataloguesStatus: (url: string, token: string) => Promise<ISyncStatuses> = (apiUrl, token) => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			const response = await fetch(apiUrl);
+			const res = await response.text() as ISyncStatuses;
 
 			resolve(res);
 		} catch (e) {
--- src/services/catalogues-api/catalogues-service.ts
+++ src/services/catalogues-api/catalogues-service.ts
@@ -1,6 +1,6 @@
 import { getToken } from "../firebase/auth";
 import { IFile } from "../firebase/db";
-import { apiExists, deleteCatalogue, uploadCatalogue } from "./catalogues-couriers";
+import { apiExists, deleteCatalogue, syncCatalogues, syncCataloguesStatus, uploadCatalogue } from "./catalogues-couriers";
 
 type ICall<T, P = void> = (url: string, token: string, ...args: T[]) => Promise<P>;
 
@@ -9,19 +9,28 @@ type ICouriers = {
 	checkExistance: ICall<undefined, boolean>;
 	upload: ICall<IFile>;
 	delete: ICall<string>;
+	sync: ICall<never, string>;
+	syncStatus: ICall<never, ISyncStatuses>;
 }
+
+export type ISyncStatuses = 'in-progress' | 'errored' | 'succeeded';
 
 class CataloguesApiServiceClass {
 	private url: string;
 	private token: string;
+	private syncCallId: string;
 	private getToken: () => Promise<string>;
 	private checkExistance: ICall<undefined, boolean>;
 	private upload: ICall<IFile>;
 	private delete: ICall<string>;
+	private syncCourier: ICall<never, string>;
+	private getSyncStatusCourier: ICall<never, ISyncStatuses>;
 
 	constructor(couriers: ICouriers) {
 		this.getToken = couriers.getToken;
 		this.checkExistance = couriers.checkExistance;
+		this.syncCourier = couriers.sync;
+		this.getSyncStatusCourier = couriers.syncStatus;
 	}
 
 	set apiUrl(url: string) {
@@ -39,6 +48,32 @@ class CataloguesApiServiceClass {
 
 		return apiExists;
 	}
+
+	public async sync(callback: (status: 'succeeded' | 'errored') => void): Promise<void> {
+		await this.refreshToken();
+
+		const token = await this.syncCourier(`${this.url}/files/sync`, this.token);
+
+		this.syncCallId = token;
+
+		this.pollSyncStatus(callback);
+	}
+
+	public async pollSyncStatus(callback: (status: 'succeeded' | 'errored') => void): Promise<void> {
+		await this.refreshToken();
+
+		const status = await this.getSyncStatusCourier(`${this.url}/files/queue/${this.syncCallId}`, this.token);
+
+		if (status === 'in-progress') {
+			setTimeout(() => {
+				this.pollSyncStatus(callback);
+			}, 500);
+
+			return;
+		}
+
+		callback(status);
+	}
 }
 
 export const CataloguesApiService = new CataloguesApiServiceClass({
@@ -46,4 +81,6 @@ export const CataloguesApiService = new CataloguesApiServiceClass({
 	checkExistance: apiExists,
 	upload: uploadCatalogue,
 	delete: deleteCatalogue,
+	sync: syncCatalogues,
+	syncStatus: syncCataloguesStatus
 });
