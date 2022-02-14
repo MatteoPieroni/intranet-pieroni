707961c4bcab1d37fda7721fa5d84a058886794c
Matteo Pieroni <matteopieroni6@gmail.com>
2022-02-14 18:34:55 +0000

    Moved internal loading to Catalogues page

--- src/pages/Catalogues.tsx
+++ src/pages/Catalogues.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from '@emotion/styled';
 import { CataloguesArchive } from '../components/catalogues-archive';
 import 'react-contexify/dist/ReactContexify.css';
+import { useConfig } from '../shared/hooks';
+import { Loader } from '../components';
 
 const StyledPage = styled.main`
   padding: 1rem;
 `;
 
 export const Catalogues: React.FC = () => {
+  const { isInternal, checkInternal } = useConfig();
+
+  useEffect(() => {
+    if (isInternal === undefined) {
+      checkInternal();
+    }
+  }, [isInternal]);
+
+  const isLoading = isInternal === undefined;
 
   return (
     <StyledPage>
-      <CataloguesArchive />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <CataloguesArchive />
+      )}
     </StyledPage>
   )
 }
