6be9506a3a4f9f5f98d09755143f1fa54cfb10de
Matteo Pieroni <matteopieroni6@gmail.com>
2024-05-20 19:02:06 +0100

    feat(Maps): read config from firebase

--- src/services/firebase/db/config/config-courier.ts
+++ src/services/firebase/db/config/config-courier.ts
@@ -6,5 +6,8 @@ export const getConfig: () => Promise<Types.IConfig> = () => getDbRecord<Types.I
 		smsApi: data.sms_api,
 		mailUrl: data.mail_url,
 		apiUrl: data.api_url,
+		transportCostPerMinute: data.transport_cost_per_minute,
+		transportCostMinimum: data.transport_cost_minimum,
+		transportHourBase: data.transport_hour_base
 	}
 });
