1f6b9653ed13546b194190164a74d78d46ee7286
Matteo Pieroni <matteopieroni6@gmail.com>
2024-12-29 14:11:48 +0000

    feat(Home): add form to home

--- src/components/forms/index.ts
+++ src/components/forms/index.ts
@@ -2,3 +2,4 @@ export { LinksForm } from './links-form/links-form';
 export { QuoteForm } from './quote-form/quote-form';
 export { SmsForm } from './sms-form';
 export { LoginForm } from './login-form';
+export { TvForm } from './tv-form';
