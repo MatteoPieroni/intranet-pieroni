a3b55f31c32fc2a72fca4722fa2860b582497eb1
Matteo Pieroni <matteopieroni6@gmail.com>
2025-01-09 16:19:11 +0000

    feat: add stars and styles

---
+++ src/components/icons/star-half.tsx
@@ -0,0 +1,27 @@
+export const StarHalfIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={24}
+    height={24}
+    fill={'none'}
+    {...props}
+  >
+    <path
+      d="M 12 2 L 12.003 19.404 C 11.643 19.403 11.283 19.484 11.012 19.646 L 8.022 21.43 C 5.872 22.71 4.583 21.763 5.143 19.313 L 5.852 16.218 C 5.982 15.633 5.752 14.817 5.333 14.393 L 2.853 11.893 C 1.383 10.421 1.863 8.929 3.903 8.586 L 7.092 8.052 C 7.632 7.961 8.272 7.487 8.512 6.993 L 10.272 3.445 C 10.746 2.484 11.372 2.002 12 2 Z"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+    <path
+      d="M 15 20.844 L 15.981 21.43 C 18.121 22.71 19.421 21.753 18.861 19.313 L 18.151 16.218 C 18.021 15.633 18.251 14.817 18.671 14.393 L 21.151 11.893 C 22.611 10.421 22.141 8.929 20.101 8.586 L 16.911 8.052 C 16.381 7.961 15.741 7.487 15.501 6.993 L 15 5.981"
+      data-stroke="empty"
+      stroke="currentColor"
+      fill="none"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    ></path>
+  </svg>
+);
