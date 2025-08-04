93c202d117281373949234a8e32ff8a9aa6ee6ec
Matteo Pieroni <matteopieroni6@gmail.com>
2025-08-04 15:26:04 +0100

    fix: add log to show when user is not needed

--- src/services/firebase/server/auth/index.ts
+++ src/services/firebase/server/auth/index.ts
@@ -18,9 +18,32 @@ export async function getUser(headers: PassedHeaders) {
   const dbRef = ref(getDatabase(firebaseServerApp));
 
   try {
+    const uid = auth.currentUser.uid;
+
+    if (!uid) {
+      throw new Error('Unknown user - no uid');
+    }
+
     const snapshot = await get(child(dbRef, `users/${auth.currentUser.uid}`));
+
     if (!snapshot.exists()) {
-      throw new Error('Unknown user');
+      const displayName = auth.currentUser.displayName || 'no-name';
+      const email = auth.currentUser.providerData[0].email || 'no-email';
+
+      const data = {
+        displayName,
+        email,
+        isAdmin: false,
+      };
+
+      if (!email.match('@pieroni.it')) {
+        throw new Error(`Email not supported: ${email} - ${uid}`);
+      }
+
+      // need to create new user, log to be checked
+      throw new Error(
+        `User needs creation. Uid: ${uid}, data: ${JSON.stringify(data)}`
+      );
     }
 
     const { nome, cognome, ...rest }: IDbUser = snapshot.val();
