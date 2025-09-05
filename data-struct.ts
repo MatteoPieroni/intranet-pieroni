1463e7ae3ac368938fe688ce76190bd84536e401
Matteo Pieroni <matteopieroni6@gmail.com>
2025-09-05 16:55:32 +0100

    feat: add data structure

---
+++ data-struct.ts
@@ -0,0 +1,37 @@
+// scopes
+
+// teams
+{
+    id: {
+        name: ''
+    }
+}
+
+// links
+{
+    teams: ['id1', 'id2'],
+    icon: 'link'
+}
+
+// user
+{
+    teams: ['id1']
+}
+
+// riscosso
+{
+    id: '',
+    date: '',
+    total: 123,
+    payment_method: 'ENUM',
+    payment_cheque_value: 123,
+    payment_cheque_number: '',
+    docs: [
+        {
+            number: '',
+            type: 'ENUM',
+            total: 123,
+            
+        }
+    ]
+}
