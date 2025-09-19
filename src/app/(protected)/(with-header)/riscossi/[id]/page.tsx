8406b8d98d72610246374503c49cceb4ddf3a7e5
Matteo Pieroni <matteopieroni6@gmail.com>
2025-09-19 15:15:05 +0100

    WIP(riscosso): print styles

--- src/app/(protected)/(with-header)/riscossi/[id]/page.tsx
+++ src/app/(protected)/(with-header)/riscossi/[id]/page.tsx
@@ -7,8 +7,14 @@ import { headers } from 'next/headers';
 import { RiscossiForm } from '@/components/riscosso-form/riscosso-form';
 
 export const metadata: Metadata = {
-  title: 'Riscossi - Intranet Pieroni srl',
-  description: 'Intranet - vedi i riscossi',
+  title: 'Riscosso - Intranet Pieroni srl',
+  description: 'Intranet - riscosso',
+};
+
+const companies = {
+  pieroni: 'Pieroni srl',
+  'pieroni-mostra': 'Pieroni in mostra',
+  pellet: 'Pellet',
 };
 
 export default async function Riscossi({
@@ -30,14 +36,91 @@ export default async function Riscossi({
   return (
     <main className={template.page}>
       <div className={template.header}>
-        <h1>Riscosso {id}</h1>
+        <h1 className={styles.noPrint}>Riscosso {id}</h1>
       </div>
 
       <div className={styles.container}>
         <div className={styles.section}>
-          {riscosso.id} - {riscosso.client} - {riscosso.client}
+          <h2 className={styles.noPrint}>Documento</h2>
+          <div className={styles.document}>
+            <div className={styles.documentRow}>
+              <h3>{companies[riscosso.company]}</h3>
+              <img src="logo.jpg" alt="" />
+            </div>
+            <div className={styles.documentRow}>
+              <p>
+                Loc. Pastino, 67 – Diecimo
+                <br />
+                55020 Borgo a Mozzano (LU)
+                <br />
+                P. IVA 01798100465
+              </p>
+
+              <table>
+                <thead>
+                  <tr>
+                    <th scope="col">Data</th>
+                    <th scope="col">Cliente</th>
+                    <th scope="col">Numero</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>{riscosso.date.toDateString()}</td>
+                    <td>{riscosso.client}</td>
+                    <th scope="row">{riscosso.id}</th>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className={styles.documentRow}>
+              <h3>Riscosso</h3>
+            </div>
+
+            <div className={styles.documentRow}>
+              <h4>Riferimento</h4>
+            </div>
+            <div className={`${styles.documentRow} ${styles.docsContainer}`}>
+              <table>
+                <thead>
+                  <tr>
+                    <th scope="col">Numero</th>
+                    <th scope="col">Data</th>
+                    <th scope="col">Tipo</th>
+                    <th scope="col">Importo</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {riscosso.docs.map((doc) => (
+                    <tr key={doc.number}>
+                      <th scope="row">{doc.number}</th>
+                      <td>{/*doc.date.toDateString()*/}</td>
+                      <td>{doc.type}</td>
+                      <td className="number">{doc.total} €</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+
+            <div className={styles.documentRow}>
+              Totale pagato {riscosso.total}
+            </div>
+            <div className={styles.documentRow}>
+              <h4>Modalità pagamento</h4>
+            </div>
+            <div className={styles.documentRow}>
+              {riscosso.paymentMethod}
+              {riscosso.paymentMethod === 'assegno' && (
+                <>
+                  N° assegno: {riscosso.paymentChequeNumber} - Importo{' '}
+                  {riscosso.paymentChequeValue}
+                </>
+              )}
+            </div>
+          </div>
         </div>
-        <div className={styles.section}>
+        <div className={`${styles.noPrint} ${styles.section}`}>
           <RiscossiForm riscosso={riscosso} />
         </div>
       </div>
