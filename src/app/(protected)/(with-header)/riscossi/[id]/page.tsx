648e7027629a3590dea48c68a7905e72723b1a22
Matteo Pieroni <matteopieroni6@gmail.com>
2025-09-22 10:54:59 +0100

    feat(riscosso): print styles

--- src/app/(protected)/(with-header)/riscossi/[id]/page.tsx
+++ src/app/(protected)/(with-header)/riscossi/[id]/page.tsx
@@ -12,10 +12,25 @@ export const metadata: Metadata = {
 };
 
 const companies = {
-  pieroni: 'Pieroni srl',
-  'pieroni-mostra': 'Pieroni in mostra',
-  pellet: 'Pellet',
+  pieroni: {
+    label: 'Pieroni srl',
+    img: 'pieroni-logo.jpg',
+  },
+  'pieroni-mostra': {
+    label: 'Pieroni in mostra',
+    img: 'pieroni-mostra-logo.jpg',
+  },
+  pellet: { label: 'Pellet', img: 'pieroni-pellet-logo.jpg' },
 };
+
+const paymentMethods = {
+  assegno: 'Assegno',
+
+  contanti: 'Contanti',
+
+  bancomat: 'Bancomat',
+};
+const documentTypes = { fattura: 'Fattura', DDT: 'DDT', impegno: 'Impegno' };
 
 export default async function Riscossi({
   params,
@@ -32,6 +47,16 @@ export default async function Riscossi({
   }
 
   const riscosso = await getRiscosso(currentHeaders, id);
+  const {
+    company,
+    date,
+    client,
+    docs,
+    total,
+    paymentMethod,
+    paymentChequeNumber,
+    paymentChequeValue,
+  } = riscosso;
 
   return (
     <main className={template.page}>
@@ -42,81 +67,88 @@ export default async function Riscossi({
       <div className={styles.container}>
         <div className={styles.section}>
           <h2 className={styles.noPrint}>Documento</h2>
-          <div className={styles.document}>
-            <div className={styles.documentRow}>
-              <h3>{companies[riscosso.company]}</h3>
-              <img src="logo.jpg" alt="" />
-            </div>
-            <div className={styles.documentRow}>
-              <p>
-                Loc. Pastino, 67 – Diecimo
-                <br />
-                55020 Borgo a Mozzano (LU)
-                <br />
-                P. IVA 01798100465
-              </p>
+          <div className={styles.documentContainer}>
+            <div className={styles.document}>
+              <div className={styles.documentRow}>
+                <p className={styles.documentTitle}>
+                  {companies[company].label}
+                </p>
+                {/* eslint-disable-next-line @next/next/no-img-element */}
+                <img src={`/assets/${companies[company].img}`} alt="" />
+              </div>
+              <div className={styles.documentRow}>
+                <p>
+                  Loc. Pastino, 67 – Diecimo
+                  <br />
+                  55020 Borgo a Mozzano (LU)
+                  <br />
+                  P. IVA 01798100465
+                </p>
 
-              <table>
-                <thead>
-                  <tr>
-                    <th scope="col">Data</th>
-                    <th scope="col">Cliente</th>
-                    <th scope="col">Numero</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>{riscosso.date.toDateString()}</td>
-                    <td>{riscosso.client}</td>
-                    <th scope="row">{riscosso.id}</th>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div className={styles.documentRow}>
-              <h3>Riscosso</h3>
-            </div>
-
-            <div className={styles.documentRow}>
-              <h4>Riferimento</h4>
-            </div>
-            <div className={`${styles.documentRow} ${styles.docsContainer}`}>
-              <table>
-                <thead>
-                  <tr>
-                    <th scope="col">Numero</th>
-                    <th scope="col">Data</th>
-                    <th scope="col">Tipo</th>
-                    <th scope="col">Importo</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {riscosso.docs.map((doc) => (
-                    <tr key={doc.number}>
-                      <th scope="row">{doc.number}</th>
-                      <td>{/*doc.date.toDateString()*/}</td>
-                      <td>{doc.type}</td>
-                      <td className="number">{doc.total} €</td>
+                <table>
+                  <thead>
+                    <tr>
+                      <th scope="col">Data</th>
+                      <th scope="col">Cliente</th>
+                      <th scope="col">Numero</th>
                     </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>{date.toDateString()}</td>
+                      <td>{client}</td>
+                      <th scope="row">{id}</th>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div className={`${styles.documentRow} ${styles.alignCenter}`}>
+                <p className={styles.documentSubtitle}>Riscosso</p>
+              </div>
 
-            <div className={styles.documentRow}>
-              Totale pagato {riscosso.total}
-            </div>
-            <div className={styles.documentRow}>
-              <h4>Modalità pagamento</h4>
-            </div>
-            <div className={styles.documentRow}>
-              {riscosso.paymentMethod}
-              {riscosso.paymentMethod === 'assegno' && (
-                <>
-                  N° assegno: {riscosso.paymentChequeNumber} - Importo{' '}
-                  {riscosso.paymentChequeValue}
-                </>
-              )}
+              <div className={styles.documentRow}>
+                <p className={styles.documentSectionTitle}>Riferimento</p>
+              </div>
+              <div className={`${styles.documentRow} ${styles.docsContainer}`}>
+                <table>
+                  <thead>
+                    <tr>
+                      <th scope="col">Numero</th>
+                      <th scope="col">Data</th>
+                      <th scope="col">Tipo</th>
+                      <th scope="col">Importo</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {docs.map((doc) => (
+                      <tr key={doc.number}>
+                        <th scope="row">{doc.number}</th>
+                        <td>{/*doc.date.toDateString()*/}</td>
+                        <td>{documentTypes[doc.type]}</td>
+                        <td className="number">{doc.total} €</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+
+              <div className={`${styles.documentRow} ${styles.alignRight}`}>
+                Totale pagato {total} €
+              </div>
+              <div className={styles.documentRow}>
+                <p className={styles.documentSectionTitle}>
+                  Modalità pagamento
+                </p>
+              </div>
+              <div className={styles.documentRow}>
+                {paymentMethods[paymentMethod]}
+                {paymentMethod === 'assegno' && (
+                  <>
+                    N° assegno: {paymentChequeNumber} - Importo{' '}
+                    {paymentChequeValue}
+                  </>
+                )}
+              </div>
             </div>
           </div>
         </div>
