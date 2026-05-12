87d7df1730dc70ac3325fd20053c5a98cdb10d05
Matteo Pieroni <matteopieroni6@gmail.com>
2026-05-12 12:38:23 +0100

    chore(transportCost): new adapter

--- src/services/gmaps/transportCost.ts
+++ src/services/gmaps/transportCost.ts
@@ -1,22 +1,24 @@
-import * as Types from "./driver/types";
+import { InputDriver, MapConfig, MapDriver } from "./driver/new-driver";
 
 // Config Object
 export const mapConfig = {
 	origins: [
 		{
 			name: "Pieroni srl, Diecimo, Lucca",
-			text: "Diecimo",
+			address: "Pieroni srl, Diecimo, Lucca",
+			id: "Diecimo",
 			coordinates: {
-				lat: 43.95545625179135,
-				lng: 10.50189202868833,
+				latitude: 43.95545625179135,
+				longitude: 10.50189202868833,
 			},
 		},
 		{
 			name: "Pieroni srl, via della Canovetta, Lucca",
-			text: "Lucca",
+			address: "Pieroni srl, via della Canovetta, Lucca",
+			id: "Lucca",
 			coordinates: {
-				lat: 43.86263827514731,
-				lng: 10.521366784508059,
+				latitude: 43.86263827514731,
+				longitude: 10.521366784508059,
 			},
 		},
 	],
@@ -27,15 +29,15 @@ export const mapConfig = {
 	},
 	mapConfig: {
 		center: {
-			lat: 43.9084255,
-			lng: 10.5158615,
+			latitude: 43.9084255,
+			longitude: 10.5158615,
 		},
 		zoom: 13,
-		mapId: "b994950262a4301394908a42",
+		mapId: "76b18e78ad699c4b99e86c27",
 	},
 	div: "map",
 	autocomplete: {
-		div: "autocomplete",
+		div: "autocomplete-container",
 		settings: {
 			componentRestrictions: {
 				country: "it",
@@ -44,29 +46,133 @@ export const mapConfig = {
 	},
 };
 
+export type Location = {
+	id: string;
+	name: string;
+	address: string;
+	coordinates: {
+		latitude: number;
+		longitude: number;
+	};
+};
+
+type Route = {
+	id: string;
+	km: number;
+	duration: number;
+	cost: string;
+};
+
+export type TransportState = {
+	routes: Route[];
+	destination: string | undefined;
+};
+
+type Listener = () => void;
+
+type Config = {
+	mapConfig: MapConfig;
+	origins: Location[];
+	costs?: CostConfig;
+};
+
+type CostConfig = {
+	costPerMinute: number;
+	hourBase: number;
+	minimumCost: number;
+};
+
 export const TransportCost = class {
-	private Driver: Types.TDriver;
-	private Listeners: Types.TListener[];
-
-	constructor(driver: Types.TDriver, config: Types.Config, maps: unknown) {
-		this.Driver = new driver(maps, config);
-		this.Driver.subscribe(this.listenToChanges);
-		this.Listeners = [];
-	}
-
-	public initialise: () => void = () => {
-		this.Driver.initAutocomplete();
-		this.Driver.initMap();
+	private InputDriver: InputDriver;
+	private MapDriver: MapDriver;
+	private Listeners: Listener[];
+	private config: Config;
+	private State: TransportState = {
+		routes: [],
+		destination: undefined,
 	};
 
-	private listenToChanges: (data: Types.TCurrent) => void = (data) =>
-		this.Listeners.forEach((listener) => listener(data));
+	constructor(
+		inputDriver: typeof InputDriver,
+		mapDriver: typeof MapDriver,
+		config: Config,
+	) {
+		this.InputDriver = new inputDriver();
+		this.MapDriver = new mapDriver();
+		this.Listeners = [];
+		this.config = config;
+	}
 
-	public subscribe: (listener: Types.TListener) => void = (listener) =>
-		(this.Listeners = [...this.Listeners, listener]);
+	public initialise = (
+		inputContainer: HTMLElement,
+		mapContainer: HTMLElement,
+		costConfig: CostConfig,
+	) => {
+		this.InputDriver.init(inputContainer, this.onInputLocation);
+		this.MapDriver.init(mapContainer, this.config.mapConfig);
+		this.config.costs = costConfig;
+	};
 
-	public unsubscribe: (listener: Types.TListener) => void = (listener) =>
-		(this.Listeners = [...this.Listeners].filter(
+	private onInputLocation = async (location: Location) => {
+		this.State.destination = location.address;
+		this.calculateTotalCost(location);
+	};
+
+	private calculateTotalCost = async (destination: Location) => {
+		const routes = await this.MapDriver.calculateDistances({
+			origins: this.config.origins,
+			destination,
+		});
+
+		const routesWithCost = routes.map((route) => ({
+			...route,
+			name: this.config.origins.find((o) => o.id === route.id)?.name,
+			cost: this.calculateCost(route.duration),
+		}));
+		const orderedRoutes = routesWithCost.sort((a, b) =>
+			a.cost.localeCompare(b.cost),
+		);
+		this.State.routes = orderedRoutes;
+
+		this.emit();
+	};
+
+	private calculateCost = (routeMinutes: number) => {
+		if (!this.config?.costs) {
+			throw new Error("Costs not configured");
+		}
+
+		const { costPerMinute, hourBase, minimumCost } = this.config.costs;
+
+		// if car is 1 minute => truck is 1.5 minutes
+		const truckMinutes = routeMinutes * 1.5;
+
+		const costToRun = truckMinutes * costPerMinute;
+
+		// Find Cost
+		const cost = costToRun + hourBase;
+		// total or minimum
+		const resultingCost = minimumCost <= cost ? cost : minimumCost;
+
+		return resultingCost.toFixed(2);
+	};
+
+	private emit = () => {
+		this.State = { ...this.State };
+		console.log("emit", { state: this.State, listeners: this.Listeners });
+		this.Listeners.forEach((listener) => listener());
+	};
+
+	public getState = () => this.State;
+
+	public subscribe = (listener: Listener) => {
+		this.Listeners = [...this.Listeners, listener];
+		return () => this.unsubscribe(listener);
+	};
+
+	public unsubscribe = (listener: Listener) => {
+		this.Listeners = [...this.Listeners].filter(
 			(inListener) => inListener.toString() === listener.toString(),
-		));
+		);
+	};
 };
