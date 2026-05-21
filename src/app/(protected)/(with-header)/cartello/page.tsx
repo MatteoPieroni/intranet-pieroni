685fbaecb5d91522abeb3883fc03da4c41d25503
Matteo Pieroni <matteopieroni6@gmail.com>
2026-05-21 18:56:20 +0100

    feat(pdf): update page

--- src/app/(protected)/(with-header)/cartello/page.tsx
+++ src/app/(protected)/(with-header)/cartello/page.tsx
@@ -1,35 +1,36 @@
-import type { Metadata } from 'next';
+import type { Metadata } from "next";
 
-import { PdfForm } from '@/components/pdf-form/pdf-form';
-import styles from './page.module.css';
-import template from '../header-template.module.css';
+import { PdfForm } from "@/components/pdf-form/pdf-form";
+import styles from "./page.module.css";
+import template from "../header-template.module.css";
+import { Surface } from "@/components/surface/surface";
 
 export const metadata: Metadata = {
-  title: 'Crea cartello - Intranet Pieroni srl',
-  description: 'Intranet - crean un cartello da stampare',
+	title: "Crea cartello - Intranet Pieroni srl",
+	description: "Intranet - crean un cartello da stampare",
 };
 
 export default async function Home() {
-  return (
-    <main className={template.page}>
-      <div className={template.header}>
-        <h1>Stampa un cartello</h1>
-      </div>
+	return (
+		<main className={template.page}>
+			<div className={template.header}>
+				<h1>Stampa un cartello</h1>
+			</div>
 
-      <div>
-        <div className={styles.container}>
-          <h2>Ricordati di rispettare questi punti:</h2>
-          <ul className={styles.list}>
-            <li>Sii concis*</li>
-            <li>
-              Comincia (dove appropriato) con &#34;Si avvisa la gentila
-              clientela...&#34;
-            </li>
-            <li>Indica che il primo e l&#39;ultimo giorno sono compresi</li>
-          </ul>
-          <PdfForm />
-        </div>
-      </div>
-    </main>
-  );
+			<div>
+				<Surface level={0} className={styles.container}>
+					<h2>Ricordati di rispettare questi punti:</h2>
+					<ul className={styles.list}>
+						<li>Sii concis*</li>
+						<li>
+							Comincia (dove appropriato) con &#34;Si avvisa la gentila
+							clientela...&#34;
+						</li>
+						<li>Indica che il primo e l&#39;ultimo giorno sono compresi</li>
+					</ul>
+					<PdfForm />
+				</Surface>
+			</div>
+		</main>
+	);
 }
